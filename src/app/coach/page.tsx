afca5b487916d3c0713e8f3d16c3f19dc183f040
tgsatwit <timothygillam@gmail.com>
2024-11-30 21:36:26 +1000

    added coach and analyzer pages

---
+++ src/app/coach/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { Conversation } from '../components/conversation';
+import RightSidebar from '../components/ai-right-sidebar';
+
+export default function Coach() {
+  return (
+    <div className="flex flex-col lg:flex-row w-full min-h-screen bg-[#0D0F10] text-white overflow-x-hidden">
+      {/* Main content area */}
+        {/* Conversation component */}
+        <Conversation />
+      {/* Right sidebar */} 
+      <RightSidebar />
+    </div>
+  );
+}
